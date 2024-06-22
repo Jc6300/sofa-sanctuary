38064b85e5d2912ebce335e1b6325d3373dab51e
Jc6300 <joshcotton93@gmail.com>
2024-06-22 18:07:55 -0500

    updated about and error page

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
@@ -50,8 +51,8 @@ const router = createBrowserRouter([
       },
     ],
   },
-  { path: "/login", element: <Login /> },
-  { path: "/register", element: <Register /> },
+  { path: "/login", element: <Login />, errorElement: <Error /> },
+  { path: "/register", element: <Register />, errorElement: <Error /> },
 ]);
 
 function App() {
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,19 @@
 function About() {
-  return <h1 className="text-4xl">About Page</h1>;
+  return (
+    <section>
+      <h1 className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl">
+        We love{" "}
+        <span className="bg-primary py-2 px-4 rounded-lg tracking-widest text-white">
+          Sofa
+        </span>
+      </h1>
+      <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
+        reprehenderit aliquid praesentium eum doloremque nesciunt ut excepturi.
+        Nesciunt amet consequuntur voluptatibus ad harum neque repudiandae,
+        obcaecati deleniti ab rem labore.
+      </p>
+    </section>
+  );
 }
 export default About;
