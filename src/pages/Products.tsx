de1725d739471d2105add4782ce0f4ab9553a6b9
Jc6300 <joshcotton93@gmail.com>
2024-06-26 15:58:39 -0500

    test

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -3,8 +3,16 @@ import { customFetch, type ProductsResponse } from "../utils";
 import { type LoaderFunction } from "react-router-dom";
 
 const url = "/products";
-export const loader: LoaderFunction = async (): Promise<ProductsResponse> => {
-  const response = await customFetch<ProductsResponse>(url);
+export const loader: LoaderFunction = async ({
+  request,
+}): Promise<ProductsResponse> => {
+  console.log(request.url);
+
+  const response = await customFetch<ProductsResponse>(url, {
+    params: {
+      search: "chair",
+    },
+  });
 
   return { ...response.data };
 };
