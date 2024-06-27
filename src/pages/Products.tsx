c026c8d3dbaa2b8d2db850e79bff2eebc8533da8
Jc6300 <joshcotton93@gmail.com>
2024-06-27 15:21:36 -0500

    updated filters and product page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,20 +1,25 @@
 import { Filters, ProductsContainer, PaginationContainer } from "@/components";
-import { customFetch, type ProductsResponse } from "../utils";
+import {
+  customFetch,
+  type ProductsResponseWithParams,
+  type ProductsResponse,
+} from "../utils";
 import { type LoaderFunction } from "react-router-dom";
 
 const url = "/products";
 export const loader: LoaderFunction = async ({
   request,
-}): Promise<ProductsResponse> => {
-  console.log(request.url);
+}): Promise<ProductsResponseWithParams> => {
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  console.log(params);
 
   const response = await customFetch<ProductsResponse>(url, {
-    params: {
-      search: "chair",
-    },
+    params,
   });
 
-  return { ...response.data };
+  return { ...response.data, params };
 };
 
 function Products() {
