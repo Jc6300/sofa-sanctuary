e80b6496c161ea548cd38474adabce7dfbfed2bd
Jc6300 <joshcotton93@gmail.com>
2024-06-30 14:53:31 -0500

    displayed cart item,price and color in cart

--- src/components/CartItemColumns.tsx
+++ src/components/CartItemColumns.tsx
@@ -1,5 +1,55 @@
-function CartItemColumns() {
-  return <div>CartItemColumns</div>;
-}
+import { formatAsDollars } from "@/utils";
+import { useAppDispatch } from "@/hooks";
+import { Button } from "./ui/button";
+import { editItem, removeItem } from "@/features/cart/cartSlice";
+import SelectProductAmount from "./SelectProductAmount";
+import { Mode } from "./SelectProductAmount";
 
-export default CartItemColumns;
+export const FirstColumn = ({
+  title,
+  image,
+}: {
+  image: string;
+  title: string;
+}) => {
+  return (
+    <img
+      src={image}
+      alt={title}
+      className="h-24 w-24 rounded-lg sm:h-32 sm:w-32 object-cover"
+    />
+  );
+};
+export const SecondColumn = ({
+  title,
+  company,
+  productColor,
+}: {
+  title: string;
+  company: string;
+  productColor: string;
+}) => {
+  return (
+    <div className="sm:ml-4 md:ml-12 ms:w-48 ">
+      <h3 className="capitalize font-medium">{title}</h3>
+      <h4 className="mt-2 capitalize text-sm">{company}</h4>
+      <p className="mt-4 text-sm capitalize flex items-center gap-x-2">
+        color:{" "}
+        <span
+          style={{
+            width: "15px",
+            height: "15px",
+            borderRadius: "50%",
+            backgroundColor: productColor,
+          }}
+        ></span>
+      </p>
+    </div>
+  );
+};
+export const ThirdColumn = () => {
+  return <h4>Third Column</h4>;
+};
+export const FourthColumn = ({ price }: { price: number }) => {
+  return <p className="font-md sm:ml-auto">{formatAsDollars(price)}</p>;
+};
--- src/components/CartItemsList.tsx
+++ src/components/CartItemsList.tsx
@@ -1,5 +1,38 @@
-const CartItemsLife = () => {
-  return <div>CartItemsLife</div>;
+import { useAppSelector } from "@/hooks";
+import { Card } from "./ui/card";
+import {
+  FirstColumn,
+  SecondColumn,
+  ThirdColumn,
+  FourthColumn,
+} from "./CartItemColumns";
+
+const CartItemsList = () => {
+  const cartItems = useAppSelector((state) => state.cartState.cartItems);
+
+  return (
+    <div>
+      {cartItems.map((cartItem) => {
+        const { cartID, title, price, amount, image, company, productColor } =
+          cartItem;
+        return (
+          <Card
+            key={cartID}
+            className="flex flex-col gap-y-4 sm:flex-row flex-wrap p-6 mb-8"
+          >
+            <FirstColumn image={image} title={title} />
+            <SecondColumn
+              title={title}
+              company={company}
+              productColor={productColor}
+            />
+            <ThirdColumn amount={amount} cartID={cartID} />
+            <FourthColumn price={price} />
+          </Card>
+        );
+      })}
+    </div>
+  );
 };
 
-export default CartItemsLife;
+export default CartItemsList;
